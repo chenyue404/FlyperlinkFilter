d15a1081195076cd25ba6b48faf31cabf9355c6c
chenyue404 <601450225@qq.com>
2021-07-06 14:49:50 +0800

    fix a bug

--- app/src/main/java/com/chenyue404/flyperlinkfilter/WebViewHook.kt
+++ app/src/main/java/com/chenyue404/flyperlinkfilter/WebViewHook.kt
@@ -31,7 +31,6 @@ class WebViewHook : IXposedHookLoadPackage {
             WebViewClient::class.java,
             object : XC_MethodHook() {
                 override fun afterHookedMethod(param: MethodHookParam) {
-                    XposedBridge.log(TAG + "setWebViewClient")
                     param.args[0] ?: return
                     val webViewClient: WebViewClient = param.args[0] as WebViewClient
                     XposedHelpers.findAndHookMethod(
@@ -43,26 +42,31 @@ class WebViewHook : IXposedHookLoadPackage {
                             override fun beforeHookedMethod(param: MethodHookParam) {
                                 val webview = param.args[0] as WebView
                                 val url = param.args[1].toString()
-                                XposedBridge.log(TAG + "shouldOverrideUrlLoading#beforeHookedMethod, url=$url")
-                                val scheme = Uri.parse(url).scheme
-                                scheme ?: return
-                                val passSchemeList = listOf("http", "https")
-                                if (!passSchemeList.contains(scheme)) {
+                                if (shouldBlock(url)) {
                                     Toast.makeText(
                                         webview.context,
-                                        "阻止打开$scheme",
+                                        "阻止打开\n$url",
                                         Toast.LENGTH_SHORT
                                     ).show()
                                     param.args[1] = ""
-                                    param.result = false
+                                    param.result = true
                                 }
                             }
-
                         }
                     )
                     param.args[0] = webViewClient
                 }
             }
         )
+    }
+
+    private fun shouldBlock(url: String): Boolean {
+//        XposedBridge.log(TAG + "shouldBlock url=$url")
+        val scheme = Uri.parse(url).scheme
+        val passSchemeList = listOf("http", "https")
+        val result = !passSchemeList.contains(scheme)
+        if (result)
+            XposedBridge.log(TAG + "阻止打开\n$url")
+        return result
     }
 }
